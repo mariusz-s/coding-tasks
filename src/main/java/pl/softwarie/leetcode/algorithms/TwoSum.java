669540390db557af0b262bc359be7ecf25dd40d5
mariusz-s <mariusz.saldak@softwarie.pl>
2020-01-06 21:05:59 +0100

    LC: Add two sum task

---
+++ src/main/java/pl/softwarie/leetcode/algorithms/TwoSum.java
@@ -0,0 +1,28 @@
+package pl.softwarie.leetcode.algorithms;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/two-sum/
+ */
+public class TwoSum {
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> cache = new HashMap<>();
+
+        if (0 == nums.length) {
+            return new int[]{};
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (cache.containsKey(nums[i])) {
+                return new int[]{cache.get(nums[i]), i};
+            }
+            cache.put(target - nums[i], i);
+        }
+
+        return new int[]{};
+    }
+}
---
+++ src/test/java/pl/softwarie/leetcode/algorithms/TwoSumTest.java
@@ -0,0 +1,34 @@
+package pl.softwarie.leetcode.algorithms;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class TwoSumTest {
+
+    @Test
+    public void twoSum1() {
+        // having
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+
+        // when
+        int[] indices = TwoSum.twoSum(nums, target);
+
+        // then
+        assertThat(indices).containsExactly(0, 1);
+    }
+
+    @Test
+    public void twoSum2() {
+        // having
+        int[] nums = new int[]{-3, 4, 3, 90};
+        int target = 0;
+
+        // when
+        int[] indices = TwoSum.twoSum(nums, target);
+
+        // then
+        assertThat(indices).containsExactly(0, 2);
+    }
+}
