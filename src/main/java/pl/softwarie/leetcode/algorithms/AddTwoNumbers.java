5751c3f4fb001943035a82466485c4dc38ce18c6
mariusz-s <mariusz.saldak@softwarie.pl>
2020-01-08 19:49:10 +0100

    LC: Add two numbers sum task

---
+++ src/main/java/pl/softwarie/leetcode/algorithms/AddTwoNumbers.java
@@ -0,0 +1,40 @@
+package pl.softwarie.leetcode.algorithms;
+
+/**
+ * https://leetcode.com/problems/add-two-numbers/submissions/
+ */
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode node = root;
+        int overflow = 0;
+        while (null != l1 || null != l2) {
+            int a = (l1 == null) ? 0 : l1.val;
+            int b = (l2 == null) ? 0 : l2.val;
+            int sum = overflow + a + b;
+            overflow = sum / 10;
+            node.next = new ListNode(sum % 10);
+            node = node.next;
+            if (null != l1) {
+                l1 = l1.next;
+            }
+            if (null != l2) {
+                l2 = l2.next;
+            }
+        }
+        if (overflow > 0) {
+            node.next = new ListNode(overflow);
+        }
+        return root.next;
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
