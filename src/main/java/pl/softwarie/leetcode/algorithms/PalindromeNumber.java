f90d7147a2a593824722bcecb74247b4c2fd05a7
mariusz-s <mariusz.saldak@softwarie.pl>
2020-01-10 00:01:29 +0100

    LC: Add palindrome number task

---
+++ src/main/java/pl/softwarie/leetcode/algorithms/PalindromeNumber.java
@@ -0,0 +1,29 @@
+package pl.softwarie.leetcode.algorithms;
+
+class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        if (x < 10) {
+            return true;
+        }
+        int[] digits = new int[(int) Math.log10(x) + 1];
+        int i = 0;
+        while (x > 0) {
+            digits[i] = (x % 10);
+            x = x / 10;
+            i++;
+        }
+        i = 0;
+        int j = digits.length - 1;
+        while (j > i) {
+            if (digits[i] != digits[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
