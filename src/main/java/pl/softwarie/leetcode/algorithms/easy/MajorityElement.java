5498ae260d613bd5e26fca36e7a261c4f026131f
mariusz-s <30019424+mariusz-s@users.noreply.github.com>
2020-03-10 23:33:24 +0100

    LC: Add MajorityElement

---
+++ src/main/java/pl/softwarie/leetcode/algorithms/easy/MajorityElement.java
@@ -0,0 +1,14 @@
+package pl.softwarie.leetcode.algorithms.easy;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/majority-element/
+ */
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
